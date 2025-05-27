bc85f05dcf42a7fec0b132b038067ff58b76b229
BurdetteLamar <burdettelamar@yahoo.com>
2025-05-27 11:45:14 -0500

    More

--- test/test_webri.rb
+++ test/test_webri.rb
@@ -10,11 +10,7 @@ class TestWebRI < Minitest::Test
     refute_nil WebRI::VERSION
   end
 
-  def zzz_test_foo
-    out = system('ruby ./bin/webri Foo')
-    puts out
-  end
-
+  # Open a webri session and yield its IO streams.
   def webri_session(name)
     command = "ruby -d bin/webri #{name}"
     Open3.popen3(command) do |stdin, stdout, stderr, wait_thread|
@@ -24,9 +20,11 @@ class TestWebRI < Minitest::Test
   end
 
   def test_file_multiple_choices
-    webri_session('ruby:c') do |stdin, stdout, stderr|
+    short_name = 'c'
+    name = "ruby:#{short_name}" # Should offer multiple choices.
+    webri_session(name) do |stdin, stdout, stderr|
       out = stdout.readpartial(4096)
-      assert_match(/file names starting with/, out)
+      assert_match(/Found \d+ file names starting with '#{short_name}'./, out)
       stdin.write_nonblock("y\n")
       out = stdout.readpartial(4096)
       assert_match(/Choose/, out)
@@ -37,22 +35,26 @@ class TestWebRI < Minitest::Test
   end
 
   def test_file_one_choice
-    webri_session('ruby:ch') do |stdin, stdout, stderr|
+    short_name = 'yji'
+    name = "ruby:#{short_name}" # Should offer one choice.
+    webri_session(name) do |stdin, stdout, stderr|
       out = stdout.readpartial(4096)
-      assert_match(/file name starting with/, out)
+      assert_match(/Found one file name starting with '#{short_name}'./, out)
       stdin.write_nonblock("y\n")
       out = stdout.readpartial(4096)
       assert_match(/Opening/, out)
     end
   end
 
-  def zzz_test_foo
-    Open3.popen3('ruby bin/webri ruby:c') do |stdin, stdout, stderr, wait_thread|
-      puts stdout.readpartial(4096)
+  def zzz_test_file_all_choices
+    name = 'ruby:nosuch' # Should offer all choices.
+    webri_session(name) do |stdin, stdout, stderr|
+      out = stdout.readpartial(4096)
+      assert_match(/no file name starting with/, out)
       stdin.write_nonblock("y\n")
-      puts stdout.readpartial(4096)
-      stdin.write_nonblock("2\n")
+      out = stdout.readpartial(4096)
+      assert_match(/Opening/, out)
     end
-
   end
+
 end
