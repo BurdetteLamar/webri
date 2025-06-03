3462dab536f7d3f1449cdda1ad051cb56d80ec96
BurdetteLamar <burdettelamar@yahoo.com>
2025-06-03 13:22:02 -0500

    More

--- test/test_webri.rb
+++ test/test_webri.rb
@@ -368,24 +368,7 @@ class TestWebRI < Minitest::Test
       },
     }
     get_test_names_for_classes
-    # Get test names for files.
-    file_locations = get_item_locations(:file)
-    file_names = file_locations.keys
-    found_names = @@test_names[:file]
-    # Find full file names matching only one name,
-    # one with single path and one with multiple paths.
-    names_to_find = {
-      single_path: :full_unique_single_path,
-      multi_path: :full_unique_multi_path,
-    }
-    find_full_names(file_locations, found_names, names_to_find)
-    # Find abbreviated file names matching only one name,
-    # one with single path and one with multiple paths.
-    names_to_find = {
-      single_path: :abbrev_unique_single_path,
-      multi_path: :abbrev_unique_multi_path,
-    }
-    find_abbrev_names(file_locations, found_names, names_to_find)
+    get_test_names_for_files
     p @@test_names[:class]
     p @@test_names[:singleton_method]
     p @@test_names[:instance_method]
@@ -466,6 +449,26 @@ class TestWebRI < Minitest::Test
       end
       break if found
     end
+  end
+
+  def get_test_names_for_files
+    file_locations = get_item_locations(:file)
+    file_names = file_locations.keys
+    found_names = @@test_names[:file]
+    # Find full file names matching only one name,
+    # one with single path and one with multiple paths.
+    names_to_find = {
+      single_path: :full_unique_single_path,
+      multi_path: :full_unique_multi_path,
+    }
+    find_full_names(file_locations, found_names, names_to_find)
+    # Find abbreviated file names matching only one name,
+    # one with single path and one with multiple paths.
+    names_to_find = {
+      single_path: :abbrev_unique_single_path,
+      multi_path: :abbrev_unique_multi_path,
+    }
+    find_abbrev_names(file_locations, found_names, names_to_find)
   end
 
   def find_full_names(locations, found_names, names_to_find)
