160396035025e0c921c26e28f2082054ea4e5e28
BurdetteLamar <burdettelamar@yahoo.com>
2026-08-08 19:48:12 +0100

    More

--- lib/doc_maker.rb
+++ lib/doc_maker.rb
@@ -28,10 +28,44 @@ class DocMaker
     <<EOT
 #{bold_italic('Introduction')}
 
-#{webri} is a console application that lets you type command-line commands
-that open Ruby documentation pages in your web browser.
+Use console application #{webri} to open a Ruby documentation page in your web browser.
 
-For example, you can type #{command('webri Array')}    
+Example commands:
+
+  #{command('webri Array')}           Open page for class #{token('Array')}.
+  #{command('webri Enumerable')}      Open page for module #{token('Enumerable')}.
+  #{command('webri Array::new')}      Open page for class #{token('Array')}; scroll to method #{token('::new')}.
+  #{command('webri Array#size')}      Open page for class #{token('Array')}; scroll to method #{token('#size')}.
+  #{command('webri ruby:README_md')}  Open page for file #{token('ruby::README_md')}.
+
+If you're not sure of exact name, #{webri} can still help:
+
+- Class/module:
+
+  #{command('webri Ar')}              Show names of all classes/modules whose names begin with #{string('Ar')}.
+  #{command('webri Enum')}            Show names of all classes/modules whose names begin with #{string('Enum')}.
+
+- Instance method:
+
+  #{command('webri Array#')}          Show names of all instance methods in class #{token('Array')}.
+  #{command('webri Array#co')}        Show names of instance methods in class #{token('Array')} whose names begin with #{string('co')}.
+  #{command('webri Enumerable#')}     Show names of all instance methods in module #{token('Enumerable')}.
+  #{command('webri Enumerable#ch')}   Show names of instance methods in module #{token('Enumerable')} whose names begin with #{string('ch')}.
+  #{command('webri #')}               Show names of all instance methods.
+
+- Singleton method:
+
+  #{command('webri File::')}          Show names of all singleton methods in class #{token('File')}.
+  #{command('webri File::ch')}        Show names of singleton methods in class #{token('File')} whose names begin with #{string('ch')}.
+  #{command('webri FileUtils::')}     Show names of all singleton methods in class #{token('FileUtils')}.
+  #{command('webri FileUtils::ch')}   Show names of singleton methods in class #{token('FileUtils')} whose names begin with #{string('ch')}.
+  #{command('webri ::')}              Show names of all singleton methods.
+
+- File:
+
+  #{command('webri ruby:')}           Show names of all files.
+  #{command('webri ruby:CO')}         Show names of files whose names begin with #{string('CO')}. 
+
 EOT
   end
 
@@ -54,10 +88,9 @@ Argument #{variable('name')} is one of four types, as determined by its prefix:
 | Ruby file        | #{tokenq('ruby:')}        | #{tokenq('ruby:syntax_rdoc')} |
 |------------------|----------------|--------------------|
 
-Note: On the command-line, your shell may require you to escape the instance method prefix:
+Note: On command-line, your shell may require you to escape instance method prefix:
 
     #{string('\\#size')} (instead of just #{string('#size')})
-
 
 EOT
   end
@@ -66,30 +99,30 @@ EOT
     <<EOT
 #{bold_italic('Files')}
 
-To get the web page for a Ruby file,
+To get web page for a Ruby file,
 type a #{variable('name')} starting with #{tokenq('ruby:')}.
 
-When the #{variable('name')} is:
+When #{variable('name')} is:
 
-- The exact name of a Ruby file (but not the beginning of other such names):
-  #{webri} opens the page for that file.
+- The exact name of a Ruby file (but not beginning of other such names):
+  #{webri} opens page for that file.
   Examples: #{tokenq('ruby:README_md')}, #{tokenq('ruby:LEGAL')}, #{tokenq('ruby:NEWS_md')}, #{tokenq('ruby:syntax_rdoc')}.
 
-- The partial name of exactly one file:
-  #{webri} asks whether to open the page for that file.
+- The abbreviated name of exactly one file:
+  #{webri} asks whether to open page for that file.
   Examples: 
  
-  - #{tokenq('ruby:maint')}              (for #{tokenq('ruby:maintainers_md')})
-  - #{tokenq('ruby:syntax/assign')}      (for #{tokenq('ruby:syntax/assignment_rdoc')})
-  - #{tokenq('ruby:language/pack')}      (for #{tokenq('ruby:language/packed_data_rdoc')})
-  - #{tokenq('ruby:contributing/build')} (for #{tokenq('ruby:contributing/building_ruby_md')})
-  - #{tokenq('ruby:optparse/arg')}       (for #{tokenq('ruby:optparse/argument_converters_rdoc')}).
+    #{tokenq('ruby:maint')}              (for #{tokenq('ruby:maintainers_md')})
+    #{tokenq('ruby:syntax/assign')}      (for #{tokenq('ruby:syntax/assignment_rdoc')})
+    #{tokenq('ruby:language/pack')}      (for #{tokenq('ruby:language/packed_data_rdoc')})
+    #{tokenq('ruby:contributing/build')} (for #{tokenq('ruby:contributing/building_ruby_md')})
+    #{tokenq('ruby:optparse/arg')}       (for #{tokenq('ruby:optparse/argument_converters_rdoc')}).
 
-- The partial name of multiple files:
+- The abbreviated name of multiple files:
   #{webri} displays those names and lets you choose.
   Examples: #{tokenq('ruby:syntax')}, #{tokenq('ruby:contrib')}, #{tokenq('ruby:lang')}, #{tokenq('ruby:jit')}.
 
-- Not the partial name of any file:
+- Not abbreviated name of any file:
   #{webri} asks whether show all file names.
   Examples: #{tokenq('ruby:xyzzy')}, #{tokenq('ruby:nosuch')}.
 
@@ -100,30 +133,30 @@ EOT
     <<EOT
 #{bold_italic('Classes and Modules')}
 
-To get the web page for a class or module,
+To get web page for a class or module,
 type a #{variable('name')} starting with a capital letter.
 
-When the #{variable('name')} is:
+When #{variable('name')} is:
 
-- The exact name of a class/module (but not the beginning of other such names):
-  #{webri} opens the page for that class/module.
+- The exact name of a class/module (but not beginning of other such names):
+  #{webri} opens page for that class/module.
   Examples: #{tokenq('Array')}, #{tokenq('Hash')}, #{tokenq('Struct')}, #{tokenq('Integer')}, #{tokenq('Symbol')}.
 
 - The abbreviated name of exactly one class/module:
-  #{webri} asks whether to open the page for that class/module.
+  #{webri} asks whether to open page for that class/module.
   Examples:
 
-  - #{tokenq('Cov')}          (for #{tokenq('Coverage')}).
-  - #{tokenq('Eng')}          (for #{tokenq('English')}).
-  - #{tokenq('Ker')}          (for #{tokenq('Kernel')}).
-  - #{tokenq('Net::HTTP::D')} (for #{tokenq('Net::HTTP::Delete')}).}
+    #{tokenq('Cov')}          (for #{tokenq('Coverage')}).
+    #{tokenq('Eng')}          (for #{tokenq('English')}).
+    #{tokenq('Ker')}          (for #{tokenq('Kernel')}).
+    #{tokenq('Net::HTTP::D')} (for #{tokenq('Net::HTTP::Delete')}).}
 
 - The abbreviated name of multiple classes/modules:
-  #{webri} asks whether to open the page for that class/module;
+  #{webri} asks whether to show names if matching class/module names;
   if Yes, #{webri} displays those names and lets you choose.
   Examples: #{tokenq('String')}, #{tokenq('Float')}, #{tokenq('Regexp')}, #{tokenq('Net::HTTP')}.
 
-- Not the abbreviated name of any class/module:
+- Not abbreviated name of any class/module:
   #{webri} asks whether show all class/module names.
   Examples: #{tokenq('Xyzzy')}, #{tokenq('Nosuch')}.
 
@@ -134,41 +167,49 @@ EOT
     <<EOT
 #{bold_italic('Instance Methods')}
 
-You can open the web page for an instance method (and scroll to that method).
+You can open web page for an instance method (and scroll to that method).
 
-#{bold('With the Class/Module Name')}
+#{bold('With Class/Module Name')}
 
-If you know the exact names of the class/module and the method,
-type the two, separated by a hash character (#{tokenq('#')})
-type the name of the class/module
+If you know exact names of class/module and instance method,
+type two, separated by a hash character (#{tokenq('#')}):
 
-#{bold('Without the Class/Module Name')}
+If you know exact name of class/module,
+but not exact name of instance method,
+type name of class/module followed by (#{tokenq('#')})
+and abbreviated name of method:
 
-If you're not sure of the class/module that has the method,
+To see names of all instance methods in a class/module,
+type name of class/module followed by (#{tokenq('#')}):
 
-To get the web page for an instance method (and scroll to that method),
+#{bold('Without Class/Module Name')}
+
+If you're not sure of class/module that has instance method,
+type (#{tokenq('#')}) followed by exact or method name:
+
+To get web page for an instance method (and scroll to that method),
 type a #{variable('name')} starting with #{tokenq('#')}.
 
-When the #{variable('name')} is:
+When #{variable('name')} is:
 
-- The exact name of an instance method (but not the beginning of other such names):
+- The exact name of an instance method (but not beginning of other such names):
 
   - If that method appears in only one class/module,
-    #{webri} opens the page for that class-module and scrolls to the method.
+    #{webri} opens page for that class-module and scrolls to method.
     Examples: #{tokenq('#abbreviate')}, #{tokenq('#mountpoint?')}, #{tokenq('#xmlschema')}.
 
-  - If the method appears in multiple classes/modules,
+  - If method appears in multiple classes/modules,
     #{webri} shows them and lets you choose; for your choice,
-    #{webri} opens the page for that class-module and scrolls to the method.
+    #{webri} opens page for that class-module and scrolls to method.
     Examples: #{tokenq('#birthtime')}, #{tokenq('#frozen?')}, #{tokenq('#owner')}.
  
-- The partial name of multiple instance methods:
+- The abbreviated name of multiple instance methods:
   #{webri} asks whether to show those names;
   if Yes, shows them and lets you choose; for your choice,
-  #{webri} opens the page for that class-module and scrolls to the method.
+  #{webri} opens page for that class-module and scrolls to method.
   Examples: #{tokenq('#alias')}, #{tokenq('#line')}, #{tokenq('#rm')}.
 
-- Not the partial name of any instance method:
+- Not abbreviated name of any instance method:
   #{webri} asks whether show all instance method names.
   Examples: #{tokenq('#xyzzy')}, #{tokenq('#nosuch')}.
 
@@ -212,6 +253,7 @@ EOT
     magenta: 35,
     cyan: 36,
     white: 37,
+    green_background: 42,
     bright_black: 90,
     bright_red: 91,
     bright_green: 92,
@@ -235,7 +277,11 @@ EOT
   end
 
   def string(s)
-    self.ansi_color("'#{s}'", :green)
+    ansi_color("'#{s}'", :green)
+  end
+
+  def command(s)
+    bold('$ ' + s)
   end
 
 end
